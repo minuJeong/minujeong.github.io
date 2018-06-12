0f54476f07398294c879ceabc581dda2d2a2c41d
Minu Jeong <minu.hanwool@gmail.com>
2018-06-12 23:23:32 +0800

    update shape

--- glsl/js/shaders.js
+++ glsl/js/shaders.js
@@ -18,8 +18,8 @@ let frag =
 `
 #define FAR 80.0
 #define MARCHING_STEPS 128
-#define MARCHING_CLAMP 0.0001
-#define NRM_OFS 0.002
+#define MARCHING_CLAMP 0.00001
+#define NRM_OFS 0.001
 #define PI 3.141592
 
 uniform vec3 L;
@@ -106,10 +106,12 @@ float world(vec3 p)
     float cy = capsule(pry, vec3(0, 1.0, 0), vec3(0, -1.0, 0), 0.25);
     float c3 = cylinder(prz, vec3(0, 0, 0.25));
     float cz = capsule(p, vec3(0, 0, -1.0), vec3(0, 0, 1.0), 0.25);
-    float c = max(blend(blend(cy, cz, 0.25), c3, 0.25), box(p, vec3(1, 1.5, 1.5)));
+    float c = max(blend(blend(cy, cz, 0.25), c3, 0.25), box(p, vec3(cos(T) * 0.25 + 1.25, 1.5, 1.5)));
 
-    vec3 p2 = rotate(translate(p, vec3(0.35, 1, 0.35)), vec3(a, 0, 0));
-    float s = sphere(p2, 0.25);
+    float sx = sphere(translate(p, vec3(cos(T), 0.25, 0.25)), 0.25);
+    float sy = sphere(translate(p, vec3(0.25, cos(T), 0.25)), 0.25);
+    float sz = sphere(translate(p, vec3(0.25, 0.25, cos(T))), 0.25);
+    float s = blend(blend(sx, sy, 0.25), sz, 0.25);
 
     return blend(c, s, 0.25);
 }
@@ -157,7 +159,7 @@ void main()
     }
     else
     {
-        gl_FragColor = vec4(0.0, 0.0, 0.0, 0.5);
+        gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
     }
 }
 `;
